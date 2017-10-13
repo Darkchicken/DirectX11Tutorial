969d390d2b19bebdd1db166e41bc038c705ddc07
Josh Shucker <joshshucker@gmail.com>
2017-10-13 11:17:10 -0700

    Initialization for texture shader is complete

--- DirectXEngine/DirectXEngine/TextureShaderClass.cpp
+++ DirectXEngine/DirectXEngine/TextureShaderClass.cpp
@@ -67,7 +67,7 @@ bool TextureShaderClass::initializeShader(ID3D11Device* device, HWND hwnd, WCHAR
 	D3D11_INPUT_ELEMENT_DESC polygonLayout[2];
 	unsigned int numElements;
 	D3D11_BUFFER_DESC matrixBufferDesc;
-	D3D11_BUFFER_DESC samplerDesc;
+	D3D11_SAMPLER_DESC samplerDesc;
 
 	//Initialize the pointers this function will use to null
 	errorMessage = 0;
@@ -162,11 +162,77 @@ bool TextureShaderClass::initializeShader(ID3D11Device* device, HWND hwnd, WCHAR
 	//Setup the description of the dynamic matrix constant buffer that is in the vertex shader
 	matrixBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
 	matrixBufferDesc.ByteWidth = sizeof(MatrixBufferType);
+	matrixBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	matrixBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	matrixBufferDesc.MiscFlags = 0;
+	matrixBufferDesc.StructureByteStride = 0;
+
+	//Create the constant buffer pointer so we can access the vertex shader constant buffer from within this class
+	result = device->CreateBuffer(&matrixBufferDesc, NULL, &m_matrixBuffer);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	//Create a texture sampler state description
+	samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDesc.MipLODBias = 0.0f;
+	samplerDesc.MaxAnisotropy = 1;
+	samplerDesc.ComparisonFunc = D3D11_COMPARISON_ALWAYS;
+	samplerDesc.BorderColor[0] = 0;
+	samplerDesc.BorderColor[1] = 0;
+	samplerDesc.BorderColor[2] = 0;
+	samplerDesc.BorderColor[3] = 0;
+	samplerDesc.MinLOD = 0;
+	samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
+
+	//Create the texture sampler state
+	result = device->CreateSamplerState(&samplerDesc, &m_sampleState);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	return true;
+
+
+
 
 }
 void TextureShaderClass::shutdownShader()
 {
+	//Release the sampler state
+	if (m_sampleState)
+	{
+		m_sampleState->Release();
+		m_sampleState = 0;
+	}
 
+	//Release the matrix constant buffer
+	if (m_matrixBuffer)
+	{
+		m_matrixBuffer->Release();
+		m_matrixBuffer = 0;
+	}
+
+	//Release the layout
+	if (m_layout)
+	{
+		m_layout->Release();
+		m_layout = 0;
+	}
+
+	//Release the pixel shader
+	if (m_pixelShader)
+	{
+		m_pixelShader->Release();
+		m_pixelShader = 0;
+	}
+
+	//Release the vertex shader
 }
 void TextureShaderClass::outputShaderErrorMessage(ID3D10Blob*, HWND, WCHAR*)
 {
