f7184170ee890a3a02e478f4934d5dfa9841c905
Darkchicken <joshshucker@hotmail.com>
2017-10-18 22:40:00 -0700

    More work on light shader class

--- DirectXEngine/DirectXEngine/LightShaderClass.cpp
+++ DirectXEngine/DirectXEngine/LightShaderClass.cpp
@@ -59,9 +59,76 @@ bool LightShaderClass::Render(ID3D11DeviceContext* deviceContext, int indexCount
 }
 
 
-bool LightShaderClass::initializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*)
+bool LightShaderClass::initializeShader(ID3D11Device* device, HWND hwnd, WCHAR* vsFilename, WCHAR* psFilename)
 {
+	HRESULT result;
+	ID3D10Blob* errorMessage;
+	ID3D10Blob* vertexShaderBuffer;
+	ID3D10Blob* pixelShaderBuffer;
+	D3D11_INPUT_ELEMENT_DESC polygonLayout[3];
+	unsigned int numElements;
+	D3D11_SAMPLER_DESC samplerDesc;
+	D3D11_BUFFER_DESC matrixBufferDesc;
+	D3D11_BUFFER_DESC lightBufferDesc;
 
+	//Initialize the pointers this function will use to null
+	errorMessage = 0;
+	vertexShaderBuffer = 0;
+	pixelShaderBuffer = 0;
+
+	//compile the vertex shader code
+	result = D3DCompileFromFile(vsFilename, NULL, NULL, "LightVertexShader", "vs_5_0", D3D10_SHADER_ENABLE_STRICTNESS, 
+		0, &vertexShaderBuffer, &errorMessage);
+	if (FAILED(result))
+	{
+		//If the shader failed to compile, it should have written something to the error message
+		if (errorMessage)
+		{
+			outputShaderErrorMessage(errorMessage, hwnd, vsFilename);
+		}
+		//If there was nothing in the error message, it simply could not find the shader file itself
+		else
+		{
+			MessageBox(hwnd, vsFilename, L"Missing Shader File", MB_OK);
+		}
+		return false;
+	}
+
+	//compile the pixel shader code
+	result = D3DCompileFromFile(psFilename, NULL, NULL, "LightPixelShader", "ps_5_0", D3D10_SHADER_ENABLE_STRICTNESS, 
+		0, & pixelShaderBuffer, &errorMessage);
+	if (FAILED(result))
+	{
+		//If the shader failed to compile, it should have written something to the error message
+		if (errorMessage)
+		{
+			outputShaderErrorMessage(errorMessage, hwnd, psFilename);
+		}
+		//If there was nothing in the error message, it simply could not find the shader file itself
+		else
+		{
+			MessageBox(hwnd, psFilename, L"Missing Shader File", MB_OK);
+		}
+		return false;
+	}
+
+	//Create the vertex shader from the buffer
+	result = device->CreateVertexShader(vertexShaderBuffer->GetBufferPointer(),vertexShaderBuffer->GetBufferSize(),NULL, &m_vertexShader);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	//Create the pixel shader from the buffer
+	result = device->CreatePixelShader(pixelShaderBuffer->GetBufferPointer(), pixelShaderBuffer->GetBufferSize(), NULL, &m_pixelShader);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	//Create the vertex input layout description
+	//This setup needs to match the VertexType structure in the ModelClass and in the shaders
+	polygonLayout[0].SemanticName = "POSITION";
 }
 void LightShaderClass::shutdownShader()
 {
