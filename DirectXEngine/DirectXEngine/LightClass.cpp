2cf67b62b15179f0653aa0a6c2e656bcfa0217c3
Josh Shucker <joshshucker@gmail.com>
2017-10-23 12:38:09 -0700

    Added Specular Lighting

--- DirectXEngine/DirectXEngine/LightClass.cpp
+++ DirectXEngine/DirectXEngine/LightClass.cpp
@@ -31,6 +31,17 @@ void LightClass::SetDirection(float x, float y, float z)
 	return;
 }
 
+void LightClass::SetSpecularColor(float red, float green, float blue, float alpha)
+{
+	m_specularColor = XMFLOAT4(red, green, blue, alpha);
+	return;
+}
+void LightClass::SetSpecularPower(float power)
+{
+	m_specularPower = power;
+	return;
+}
+
 XMFLOAT4 LightClass::GetAmbientColor()
 {
 	return m_ambientColor;
@@ -42,4 +53,13 @@ XMFLOAT4 LightClass::GetDiffuseColor()
 XMFLOAT3 LightClass::GetDirection()
 {
 	return m_direction;
+}
+
+XMFLOAT4 LightClass::GetSpecularColor()
+{
+	return m_specularColor;
+}
+float LightClass::GetSpecularPower()
+{
+	return m_specularPower;
 }
