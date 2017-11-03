7f70e2d68011d7b5f05ea00781cbb0adf7905da5
Josh Shucker <joshshucker@gmail.com>
2017-11-02 17:07:37 -0700

    Set up text class

--- DirectXEngine/DirectXEngine/TextClass.cpp
+++ DirectXEngine/DirectXEngine/TextClass.cpp
@@ -6,7 +6,42 @@ TextClass::TextClass()
 {
 }
 
+TextClass::TextClass(const TextClass& other)
+{
+}
+
 
 TextClass::~TextClass()
 {
+}
+
+bool TextClass::Initialize(ID3D11Device*, ID3D11DeviceContext*, HWND, int, int, XMMATRIX)
+{
+
+}
+void TextClass::Shutdown()
+{
+
+}
+bool TextClass::Render(ID3D11DeviceContext*, XMMATRIX, XMMATRIX)
+{
+
+}
+
+
+bool TextClass::initializeSentence(SentenceType**, int, ID3D11Device*)
+{
+
+}
+bool TextClass::updateSentence(SentenceType*, char*, int, int, float, float, float, ID3D11DeviceContext*)
+{
+
+}
+void TextClass::releaseSentence(SentenceType**)
+{
+
+}
+bool TextClass::renderSentence(ID3D11DeviceContext*, SentenceType*, XMMATRIX, XMMATRIX)
+{
+
 }
--- DirectXEngine/DirectXEngine/TextClass.h
+++ DirectXEngine/DirectXEngine/TextClass.h
@@ -1,8 +1,48 @@
-#pragma once
+#ifndef _TEXTCLASS_H_
+#define _TEXTCLASS_H_
+
+#include "FontClass.h"
+#include "FontShaderClass.h"
+
 class TextClass
 {
+private:
+	struct SentenceType
+	{
+		ID3D11Buffer *vertexBuffer, *indexBuffer;
+		int vertexCount, indexCount, maxLength;
+		float red, green, blue;
+	};
+
+	struct VertexType
+	{
+		XMFLOAT3 position;
+		XMFLOAT2 texture;
+	};
+
 public:
 	TextClass();
+	TextClass(const TextClass&);
 	~TextClass();
+
+	bool Initialize(ID3D11Device*, ID3D11DeviceContext*, HWND, int, int, XMMATRIX);
+	void Shutdown();
+	bool Render(ID3D11DeviceContext*, XMMATRIX, XMMATRIX);
+
+private:
+	bool initializeSentence(SentenceType**, int, ID3D11Device*);
+	bool updateSentence(SentenceType*, char*, int, int, float, float, float, ID3D11DeviceContext*);
+	void releaseSentence(SentenceType**);
+	bool renderSentence(ID3D11DeviceContext*, SentenceType*, XMMATRIX, XMMATRIX);
+
+private:
+	FontClass* m_font;
+	FontShaderClass* m_fontShader;
+	int m_screenWidth, m_screenHeight;
+	XMMATRIX m_baseViewMatrix;
+
+	SentenceType* m_sentence1;
+	SentenceType* m_sentence2;
 };
+#endif
 
