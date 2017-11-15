86652bd18ca68ad698394ac79f26dca37425e74b
Josh Shucker <joshshucker@gmail.com>
2017-11-14 17:50:46 -0800

    Created position class for camera rotation

--- DirectXEngine/DirectXEngine/PositionClass.h
+++ DirectXEngine/DirectXEngine/PositionClass.h
@@ -1,8 +1,23 @@
 #pragma once
+
+#include <math.h>
+
 class PositionClass
 {
 public:
 	PositionClass();
+	PositionClass(const PositionClass&);
 	~PositionClass();
+
+	void SetFrameTime(float);
+	void GetRotation(float&, float&);
+
+	void TurnLeft(bool);
+	void TurnRight(bool);
+
+private:
+	float m_frameTime;
+	float m_rotationX, m_rotationY;
+	float m_leftTurnSpeed, m_rightTurnSpeed, m_upTurnSpeed, m_downTurnSpeed;
 };
 
