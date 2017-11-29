86a6cc76c23adb0fc3f184e1b574480c2b30f925
Josh Shucker <joshshucker@gmail.com>
2017-11-29 09:42:47 -0800

    Alpha mapping complete

--- DirectXEngine/DirectXEngine/AlphaMapShaderClass.cpp
+++ DirectXEngine/DirectXEngine/AlphaMapShaderClass.cpp
@@ -135,13 +135,13 @@ bool AlphaMapShaderClass::initializeShader(ID3D11Device* device, HWND hwnd, WCHA
 	polygonLayout[0].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
 	polygonLayout[0].InstanceDataStepRate = 0;
 
-	polygonLayout[0].SemanticName = "TEXCOORD";
-	polygonLayout[0].SemanticIndex = 0;
-	polygonLayout[0].Format = DXGI_FORMAT_R32G32_FLOAT;
-	polygonLayout[0].InputSlot = 0;
-	polygonLayout[0].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
-	polygonLayout[0].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
-	polygonLayout[0].InstanceDataStepRate = 0;
+	polygonLayout[1].SemanticName = "TEXCOORD";
+	polygonLayout[1].SemanticIndex = 0;
+	polygonLayout[1].Format = DXGI_FORMAT_R32G32_FLOAT;
+	polygonLayout[1].InputSlot = 0;
+	polygonLayout[1].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+	polygonLayout[1].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[1].InstanceDataStepRate = 0;
 
 	//Get a count of the elements in the layout
 	numElements = sizeof(polygonLayout)/ sizeof(polygonLayout[0]);
