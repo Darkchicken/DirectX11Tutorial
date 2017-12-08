e4a9e840032f913d547fdd0721edd6c5dcf4a4f8
Josh Shucker <joshshucker@gmail.com>
2017-12-07 17:03:17 -0800

    BumpMapShaderClass complete

--- DirectXEngine/DirectXEngine/BumpMapShaderClass.h
+++ DirectXEngine/DirectXEngine/BumpMapShaderClass.h
@@ -10,8 +10,45 @@ using namespace DirectX;
 
 class BumpMapShaderClass
 {
+private:
+	struct MatrixBufferType
+	{
+		XMMATRIX world;
+		XMMATRIX view;
+		XMMATRIX projection;
+	};
+
+	struct LightBufferType
+	{
+		XMFLOAT4 diffuseColor;
+		XMFLOAT3 lightDirection;
+		float padding;
+	};
+
 public:
 	BumpMapShaderClass();
+	BumpMapShaderClass(const BumpMapShaderClass&);
 	~BumpMapShaderClass();
+
+	bool Initialize(ID3D11Device*, HWND);
+	void Shutdown();
+	bool Render(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView**, XMFLOAT3, XMFLOAT4);
+
+private:
+	bool initializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*);
+	void shutdownShader();
+	void outputShaderErrorMessage(ID3D10Blob*, HWND, WCHAR*);
+
+	bool setShaderParameters(ID3D11DeviceContext*, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView**, XMFLOAT3, XMFLOAT4);
+	void renderShader(ID3D11DeviceContext*, int);
+
+private:
+	ID3D11VertexShader* m_vertexShader;
+	ID3D11PixelShader* m_pixelShader;
+	ID3D11InputLayout* m_layout;
+	ID3D11Buffer* m_matrixBuffer;
+	ID3D11SamplerState* m_sampleState;
+	ID3D11Buffer* m_lightBuffer;
+
 };
 
